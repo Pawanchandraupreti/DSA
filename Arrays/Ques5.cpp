fa58748dfa49653c51709c0c43a7cb6b2a948005
Pawan <upretipawanchandra@gmail.com>
2026-01-29 23:58:28 +0530

    new file:   Arrays/Ques5.cpp

---
+++ Arrays/Ques5.cpp
@@ -0,0 +1,17 @@
+//Write a C++ program to generate the Fibonacci series up to n terms.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a = 0, b = 1;
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+}
