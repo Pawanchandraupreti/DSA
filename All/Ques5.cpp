99b7558382c1220a42748897c433b9a1beb4f24b
Pawan <upretipawanchandra@gmail.com>
2026-02-01 23:23:11 +0530

    new file:   All/Ques5.cpp

---
+++ All/Ques5.cpp
@@ -0,0 +1,16 @@
+// Factorial of a Number
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << fact;
+    return 0;
+}
