84e8f72a382ce85f749d3547f389a08be95024d5
Pawan <upretipawanchandra@gmail.com>
2026-02-02 23:52:40 +0530

    new file:   All/Ques9.cpp

---
+++ All/Ques9.cpp
@@ -0,0 +1,18 @@
+// Fibonacci Series (first n terms)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int a = 0, b = 1;
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
