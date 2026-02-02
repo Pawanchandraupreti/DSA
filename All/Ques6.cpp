09fb04ee9a34e031a43f27dd97bc3f1b4c0c0490
Pawan <upretipawanchandra@gmail.com>
2026-02-02 23:50:29 +0530

    new file:   All/Ques6.cpp

---
+++ All/Ques6.cpp
@@ -0,0 +1,24 @@
+// Find Second Largest Element
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 5, 20, 8};
+    int n = 4;
+
+    int first = INT_MIN, second = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    cout << second;
+    return 0;
+}
