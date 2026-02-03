752e5467ec4fc2dc38d8b8872704a6efb27575a4
Pawan <upretipawanchandra@gmail.com>
2026-02-03 23:22:48 +0530

    ques 12 done

---
+++ All/Set_2/ques12.cpp
@@ -0,0 +1,18 @@
+// Find Missing Number (1 to n)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 4, 5};
+    int n = 5;
+
+    int sum = n * (n + 1) / 2;
+    int arrSum = 0;
+
+    for (int i = 0; i < n - 1; i++)
+        arrSum += arr[i];
+
+    cout << sum - arrSum;
+    return 0;
+}
