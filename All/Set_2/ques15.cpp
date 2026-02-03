039def310800f9801d79a42d5f8bb41f9b36f02b
Pawan <upretipawanchandra@gmail.com>
2026-02-03 23:24:09 +0530

    ques 15 done

---
+++ All/Set_2/ques15.cpp
@@ -0,0 +1,22 @@
+// Check if Array is a Rotation of Sorted Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {3, 4, 5, 1, 2};
+    int n = 5;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > arr[(i + 1) % n])
+            count++;
+    }
+
+    if (count == 1)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
