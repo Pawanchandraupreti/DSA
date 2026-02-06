e27a218864cd19c953d55c312f606ea7a878bd4c
Pawan <upretipawanchandra@gmail.com>
2026-02-06 23:32:34 +0530

    new file:   All/Set_2/ques17.cpp 	new file:   All/Set_2/ques17.exe

---
+++ All/Set_2/ques17.cpp
@@ -0,0 +1,27 @@
+// Find Union of Two Sorted Arrays
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[] = {1, 2, 4, 5};
+    int b[] = {2, 3, 5, 6};
+    int n = 4, m = 4;
+
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (a[i] < b[j])
+            cout << a[i++] << " ";
+        else if (a[i] > b[j])
+            cout << b[j++] << " ";
+        else {
+            cout << a[i] << " ";
+            i++; j++;
+        }
+    }
+
+    while (i < n) cout << a[i++] << " ";
+    while (j < m) cout << b[j++] << " ";
+
+    return 0;
+}
