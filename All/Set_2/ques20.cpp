2b5807fba87b9dcbbfd201c4b883f823b8969690
Pawan <upretipawanchandra@gmail.com>
2026-02-06 23:33:42 +0530

    new file:   All/Set_2/ques20.cpp 	new file:   All/Set_2/ques20.exe

---
+++ All/Set_2/ques20.cpp
@@ -0,0 +1,25 @@
+// Check if Two Strings Are Anagrams
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1 = "listen", s2 = "silent";
+    int freq[26] = {0};
+
+    for (char c : s1) freq[c - 'a']++;
+    for (char c : s2) freq[c - 'a']--;
+
+    bool ok = true;
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            ok = false;
+            break;
+        }
+    }
+
+    if (ok) cout << "Anagram";
+    else cout << "Not Anagram";
+
+    return 0;
+}
