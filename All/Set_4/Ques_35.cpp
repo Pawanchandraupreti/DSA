010c6650f3322f73e3d8489059eb7038f329d9ee
Pawan <upretipawanchandra@gmail.com>
2026-02-10 20:26:39 +0530

    new file:   All/Set_4/Ques_35.cpp

---
+++ All/Set_4/Ques_35.cpp
@@ -0,0 +1,22 @@
+// Find Square Root (Binary Search)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 25;
+    int l = 0, r = x, ans = 0;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (mid * mid <= x) {
+            ans = mid;
+            l = mid + 1;
+        } else
+            r = mid - 1;
+    }
+
+    cout << ans;
+    return 0;
+}
