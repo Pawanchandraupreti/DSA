020da21544d6f2e3c8acc3f7da978e6adbccb133
Pawan <upretipawanchandra@gmail.com>
2026-02-10 20:26:17 +0530

    new file:   All/Set_4/Ques_33.cpp

---
+++ All/Set_4/Ques_33.cpp
@@ -0,0 +1,26 @@
+// Find Last Occurrence of an Element
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 2, 2, 3};
+    int n = 5, key = 2;
+
+    int l = 0, r = n - 1, ans = -1;
+
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == key) {
+            ans = mid;
+            l = mid + 1;
+        } else if (arr[mid] < key)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+
+    cout << ans;
+    return 0;
+}
