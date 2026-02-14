32a09774ff12357ed46764d757442205595bcec3
Pawan <upretipawanchandra@gmail.com>
2026-02-14 23:17:01 +0530

    new file:   All/Set_5/Ques_45.cpp

---
+++ All/Set_5/Ques_45.cpp
@@ -0,0 +1,37 @@
+// Valid Parentheses (Using Stack)
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    string s = "{[()]}";
+    stack<char> st;
+    bool valid = true;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[')
+            st.push(c);
+        else {
+            if (st.empty()) {
+                valid = false;
+                break;
+            }
+
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                valid = false;
+                break;
+            }
+        }
+    }
+
+    if (!st.empty()) valid = false;
+
+    cout << (valid ? "Valid" : "Invalid");
+    return 0;
+}
