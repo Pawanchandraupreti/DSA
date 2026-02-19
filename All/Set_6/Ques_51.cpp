70a657fa4a4c2197d5aa017d20efb167de477abc
Pawan <upretipawanchandra@gmail.com>
2026-02-19 23:00:21 +0530

    new file:   All/Set_6/Ques_51.cpp

---
+++ All/Set_6/Ques_51.cpp
@@ -0,0 +1,25 @@
+// Container With Most Water (Two Pointers)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height[] = {1,8,6,2,5,4,8,3,7};
+    int n = 9;
+
+    int l = 0, r = n - 1;
+    int maxArea = 0;
+
+    while (l < r) {
+        int area = min(height[l], height[r]) * (r - l);
+        maxArea = max(maxArea, area);
+
+        if (height[l] < height[r])
+            l++;
+        else
+            r--;
+    }
+
+    cout << maxArea;
+    return 0;
+}
