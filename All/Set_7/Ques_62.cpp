47e157bb06192151ef70f329a8afa2cf8a7b2d7a
Pawan <upretipawanchandra@gmail.com>
2026-02-25 23:31:58 +0530

    new file:   All/Set_7/Ques_62.cpp

---
+++ All/Set_7/Ques_62.cpp
@@ -0,0 +1,25 @@
+// Generate All Permutations
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void permute(vector<int>& nums, int index) {
+    if (index == nums.size()) {
+        for (int x : nums) cout << x << " ";
+        cout << endl;
+        return;
+    }
+
+    for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        permute(nums, index + 1);
+        swap(nums[index], nums[i]); // backtrack
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    permute(nums, 0);
+    return 0;
+}
