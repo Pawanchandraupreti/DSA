14fa723d28cd59b3d26eb19a65865d29940d16f5
Pawan <upretipawanchandra@gmail.com>
2026-03-06 23:31:26 +0530

    new file:   All/Set_8/Ques_80.cpp

---
+++ All/Set_8/Ques_80.cpp
@@ -0,0 +1,32 @@
+// Minimum Path Sum (Grid DP)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grid[3][3] = {
+        {1,3,1},
+        {1,5,1},
+        {4,2,1}
+    };
+
+    int m=3,n=3;
+
+    int dp[3][3];
+
+    dp[0][0]=grid[0][0];
+
+    for(int i=1;i<m;i++)
+        dp[i][0]=dp[i-1][0]+grid[i][0];
+
+    for(int j=1;j<n;j++)
+        dp[0][j]=dp[0][j-1]+grid[0][j];
+
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+
+    cout<<dp[m-1][n-1];
+}
