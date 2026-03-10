d45d8be5dfc4218435feb7173a9e0b61443ea2b4
Pawan <upretipawanchandra@gmail.com>
2026-03-10 23:52:35 +0530

    new file:   All/Set_9/Ques_84.cpp

---
+++ All/Set_9/Ques_84.cpp
@@ -0,0 +1,30 @@
+// Activity Selection (Greedy)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int INF = 1e9;
+
+    int dist[4][4] = {
+        {0,3,INF,7},
+        {8,0,2,INF},
+        {5,INF,0,1},
+        {2,INF,INF,0}
+    };
+
+    int V=4;
+
+    for(int k=0;k<V;k++)
+        for(int i=0;i<V;i++)
+            for(int j=0;j<V;j++)
+                if(dist[i][k]+dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+
+    for(int i=0;i<V;i++){
+        for(int j=0;j<V;j++)
+            cout<<dist[i][j]<<" ";
+        cout<<endl;
+    }
+}
