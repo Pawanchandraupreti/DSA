d78b47b3a1119631a44e55cd1cb47b45dcda8050
Pawan <upretipawanchandra@gmail.com>
2026-03-10 23:50:40 +0530

    new file:   All/Set_9/Ques_81.exe

---
+++ All/Set_9/Ques_82.cpp
@@ -0,0 +1,34 @@
+// 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Edge {
+    int u,v,w;
+};
+
+int main() {
+
+    int V = 5;
+    
+    vector<Edge> edges = {
+        {0,1,5},
+        {1,2,-2},
+        {2,3,3},
+        {3,4,1}
+    };
+
+    vector<int> dist(V,1e9);
+    dist[0]=0;
+
+    for(int i=0;i<V-1;i++){
+        for(auto e:edges){
+            if(dist[e.u]+e.w < dist[e.v])
+                dist[e.v]=dist[e.u]+e.w;
+        }
+    }
+
+    for(int i=0;i<V;i++)
+        cout<<dist[i]<<" ";
+}
