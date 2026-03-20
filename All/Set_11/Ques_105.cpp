95053a70af8c5b58a4517ea065569f468ec3d707
Pawan <upretipawanchandra@gmail.com>
2026-03-20 23:49:10 +0530

    new file:   All/Set_11/Ques_105.cpp

---
+++ All/Set_11/Ques_105.cpp
@@ -0,0 +1,42 @@
+// Minimum Days to Make Bouquets
+
+#include <iostream>
+using namespace std;
+
+bool canMake(int bloom[], int n, int m, int k, int day) {
+    int flowers = 0, bouquets = 0;
+
+    for(int i=0;i<n;i++) {
+        if(bloom[i] <= day) {
+            flowers++;
+            if(flowers == k) {
+                bouquets++;
+                flowers = 0;
+            }
+        }
+        else
+            flowers = 0;
+    }
+
+    return bouquets >= m;
+}
+
+int main() {
+    int bloom[] = {1,10,3,10,2};
+    int n = 5, m = 3, k = 1;
+
+    int low = 1, high = 10, ans = -1;
+
+    while(low <= high) {
+        int mid = (low + high)/2;
+
+        if(canMake(bloom,n,m,k,mid)) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+
+    cout << ans;
+}
