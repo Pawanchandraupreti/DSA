64aec459e62fb56d40cdc844b9c6bf03d6e01d72
Pawan <upretipawanchandra@gmail.com>
2026-03-20 23:48:44 +0530

    new file:   All/Set_11/Ques_104.cpp

---
+++ All/Set_11/Ques_104.cpp
@@ -0,0 +1,38 @@
+// Split Array Largest Sum
+
+#include <iostream>
+using namespace std;
+
+bool check(int arr[], int n, int k, int maxSum) {
+    int parts = 1, sum = 0;
+
+    for(int i=0;i<n;i++) {
+        if(sum + arr[i] > maxSum) {
+            parts++;
+            sum = arr[i];
+        }
+        else
+            sum += arr[i];
+    }
+    return parts <= k;
+}
+
+int main() {
+    int arr[] = {7,2,5,10,8};
+    int n = 5, k = 2;
+
+    int low = 0, high = 32, ans;
+
+    while(low <= high) {
+        int mid = (low + high)/2;
+
+        if(check(arr,n,k,mid)) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+
+    cout << ans;
+}
