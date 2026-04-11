876492bb704e09752c39ed4e33b7b0b4451ea1f1
Pawan <upretipawanchandra@gmail.com>
2026-04-11 23:56:14 +0530

    done and 126

---
+++ All/Set_13/Ques_126.cpp
@@ -0,0 +1,36 @@
+// Binary Indexed Tree (Fenwick Tree) - Prefix Sum Query
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void update(vector<int>& bit, int idx, int val, int n) {
+    while(idx <= n) {
+        bit[idx] += val;
+        idx += idx & -idx;
+    }
+}
+
+int query(vector<int>& bit, int idx) {
+    int sum = 0;
+    while(idx > 0) {
+        sum += bit[idx];
+        idx -= idx & -idx;
+    }
+    return sum;
+}
+
+int main() {
+    vector<int> arr = {2, 1, 1, 3, 2, 3, 4, 5, 6, 7};
+    int n = arr.size();
+
+    vector<int> bit(n + 1, 0);
+    for(int i = 0; i < n; i++) {
+        update(bit, i + 1, arr[i], n);
+    }
+
+    cout << query(bit, 5) << "\n";
+
+    update(bit, 3, 4, n);
+    cout << query(bit, 5);
+}
