d644b6d9883807d79631e879286d6ab8caec2691
Pawan <upretipawanchandra@gmail.com>
2026-04-14 23:46:13 +0530

    done

---
+++ All/Set_14/Ques_134.cpp
@@ -0,0 +1,32 @@
+// Sparse Table (Range Minimum Query)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {7, 2, 3, 0, 5, 10, 3, 12, 18};
+    int n = arr.size();
+
+    int LOG = 0;
+    while((1 << LOG) <= n) LOG++;
+
+    vector<vector<int>> st(LOG, vector<int>(n));
+    for(int i = 0; i < n; i++) st[0][i] = arr[i];
+
+    for(int j = 1; j < LOG; j++) {
+        for(int i = 0; i + (1 << j) <= n; i++) {
+            st[j][i] = min(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+        }
+    }
+
+    auto rangeMin = [&](int l, int r) {
+        int len = r - l + 1;
+        int j = 0;
+        while((1 << (j + 1)) <= len) j++;
+        return min(st[j][l], st[j][r - (1 << j) + 1]);
+    };
+
+    cout << rangeMin(0, 4) << "\n";
+    cout << rangeMin(4, 7);
+}
