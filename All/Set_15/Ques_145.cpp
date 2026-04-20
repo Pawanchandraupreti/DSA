6e6dfd22816886f820beb52ca57276f9abc630cb
Pawan <upretipawanchandra@gmail.com>
2026-04-20 23:38:46 +0530

    new file:   All/Set_15/Ques_145.cpp

---
+++ All/Set_15/Ques_145.cpp
@@ -0,0 +1,28 @@
+// Longest Substring Without Repeating Characters
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s = "abcabcbb";
+    
+    unordered_map<char, int> charIndex;
+    int maxLen = 0;
+    int left = 0;
+    
+    for(int right = 0; right < s.length(); right++) {
+        if(charIndex.find(s[right]) != charIndex.end() && charIndex[s[right]] >= left) {
+            left = charIndex[s[right]] + 1;
+        }
+        charIndex[s[right]] = right;
+        maxLen = max(maxLen, right - left + 1);
+    }
+    
+    cout << "String: " << s << endl;
+    cout << "Length of Longest Substring Without Repeating Characters: " << maxLen << endl;
+    
+    return 0;
+}
