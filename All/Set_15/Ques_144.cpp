c47f14005a430dda9aa024506fccdeb068379d06
Pawan <upretipawanchandra@gmail.com>
2026-04-20 23:38:28 +0530

    new file:   All/Set_15/Ques_144.cpp

---
+++ All/Set_15/Ques_144.cpp
@@ -0,0 +1,38 @@
+// Rotate Matrix 90 Degrees Clockwise
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    
+    int n = matrix.size();
+    
+    // Transpose the matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    
+    // Reverse each row
+    for(int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+    
+    cout << "Rotated Matrix:" << endl;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
