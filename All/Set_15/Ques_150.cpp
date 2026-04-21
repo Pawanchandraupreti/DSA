95cf39af600fd699e414020ebae6e9678ff38d83
Pawan <upretipawanchandra@gmail.com>
2026-04-21 23:48:06 +0530

    Done

---
+++ All/Set_15/Ques_150.cpp
@@ -0,0 +1,39 @@
+// Check if String is Palindrome
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool isPalindrome(string s) {
+    int left = 0, right = s.length() - 1;
+    
+    while(left < right) {
+        while(left < right && !isalnum(s[left])) {
+            left++;
+        }
+        while(left < right && !isalnum(s[right])) {
+            right--;
+        }
+        
+        if(tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+        
+        left++;
+        right--;
+    }
+    
+    return true;
+}
+
+int main() {
+    string str = "A man, a plan, a canal: Panama";
+    
+    bool result = isPalindrome(str);
+    
+    cout << "String: " << str << endl;
+    cout << "Is Palindrome: " << (result ? "Yes" : "No") << endl;
+    
+    return 0;
+}
