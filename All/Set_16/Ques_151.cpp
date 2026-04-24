782a4b7774d8a929f8857957e3fceb2e01c017d7
Pawan <upretipawanchandra@gmail.com>
2026-04-24 23:51:24 +0530

    done

---
+++ All/Set_16/Ques_151.cpp
@@ -0,0 +1,23 @@
+// Reverse an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    cout << "Reversed Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
