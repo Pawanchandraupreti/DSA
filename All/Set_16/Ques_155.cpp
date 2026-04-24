6469f47aa15e7ee2fc77914b39770a3dccdeefdd
Pawan <upretipawanchandra@gmail.com>
2026-04-24 23:52:11 +0530

    done

---
+++ All/Set_16/Ques_155.cpp
@@ -0,0 +1,38 @@
+// Rotate Array by K positions
+
+#include <iostream>
+using namespace std;
+
+void rotateArray(int arr[], int n, int k) {
+    k = k % n;
+    
+    for (int i = 0; i < k; i++) {
+        int temp = arr[0];
+        for (int j = 0; j < n - 1; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr[n - 1] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+    int k = 2;
+    
+    cout << "Original Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    rotateArray(arr, n, k);
+    
+    cout << "Array after rotating by " << k << " positions: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
