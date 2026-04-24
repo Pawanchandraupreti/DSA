09794185d386042a6bbf84d805e2016ae949e0c1
Pawan <upretipawanchandra@gmail.com>
2026-04-24 23:51:48 +0530

    done

---
+++ All/Set_16/Ques_153.cpp
@@ -0,0 +1,18 @@
+// Sum of All Elements in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    
+    cout << "Sum of Array: " << sum << endl;
+    
+    return 0;
+}
