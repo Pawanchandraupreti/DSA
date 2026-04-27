a0c079564f2b35c7dd3944b16b52389be95304ee
Pawan <upretipawanchandra@gmail.com>
2026-04-27 23:19:37 +0530

    Done

---
+++ All/Set_16/Ques_158.cpp
@@ -0,0 +1,23 @@
+// Left Rotate Array by One Position
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int first = arr[0];
+    for (int i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    arr[n - 1] = first;
+
+    cout << "Array after left rotation by one position: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
