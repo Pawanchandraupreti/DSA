b4d707d12820a5de724d5b6d169d2141c539530a
Pawan <upretipawanchandra@gmail.com>
2026-04-27 23:19:37 +0530

    Done

---
+++ All/Set_16/Ques_156.cpp
@@ -0,0 +1,20 @@
+// Find Maximum Element in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {8, 21, 4, 19, 33, 10};
+    int n = 6;
+    int maximum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maximum) {
+            maximum = arr[i];
+        }
+    }
+
+    cout << "Maximum Element: " << maximum << endl;
+
+    return 0;
+}
