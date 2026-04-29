fc2f7db91defbada5ca2b1a8472e606105c865e3
Pawan <upretipawanchandra@gmail.com>
2026-04-29 23:00:34 +0530

    Done

---
+++ All/Set_17/Ques_164.cpp
@@ -0,0 +1,19 @@
+// Find Missing Number in 1 to n
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 4, 5, 6};
+    int n = 5;
+    int total = (n + 1) * (n + 2) / 2;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    cout << "Missing Number: " << total - sum << endl;
+
+    return 0;
+}
