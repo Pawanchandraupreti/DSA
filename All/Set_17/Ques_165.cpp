668a6401b08cfea8940061ba97db86a9fe720fe5
Pawan <upretipawanchandra@gmail.com>
2026-04-29 23:01:09 +0530

    Done

---
+++ All/Set_17/Ques_165.cpp
@@ -0,0 +1,27 @@
+// Maximum Subarray Sum using Kadane's Algorithm
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = 9;
+
+    int currentSum = 0;
+    int maximumSum = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        currentSum += arr[i];
+        if (currentSum > maximumSum) {
+            maximumSum = currentSum;
+        }
+        if (currentSum < 0) {
+            currentSum = 0;
+        }
+    }
+
+    cout << "Maximum Subarray Sum: " << maximumSum << endl;
+
+    return 0;
+}
