26d765be94f3a5a2933acca26481b7f275dc5025
Pawan <upretipawanchandra@gmail.com>
2026-05-01 23:07:40 +0530

    Ques 168

---
+++ All/Set_17/Ques_168.cpp
@@ -0,0 +1,21 @@
+// Count Frequency of an Element in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 2, 4, 2, 5, 2};
+    int n = 8;
+    int target = 2;
+    int frequency = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            frequency++;
+        }
+    }
+
+    cout << "Frequency of " << target << " is: " << frequency << endl;
+
+    return 0;
+}
