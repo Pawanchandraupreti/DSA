8695969fb8507acec669a1a9278ac8db2226e5f2
Pawan <upretipawanchandra@gmail.com>
2026-05-01 23:06:18 +0530

    Ques 166

---
+++ All/Set_17/Ques_166.cpp
@@ -0,0 +1,26 @@
+// Linear Search in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 12, 7, 19, 3, 11};
+    int n = 6;
+    int target = 19;
+    int index = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1) {
+        cout << "Element found at index: " << index << endl;
+    } else {
+        cout << "Element not found." << endl;
+    }
+
+    return 0;
+}
