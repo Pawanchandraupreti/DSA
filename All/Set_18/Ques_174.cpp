306780354bd267b8ed686e2446fa21db4a73b494
Pawan <upretipawanchandra@gmail.com>
2026-05-04 23:17:41 +0530

    Done

---
+++ All/Set_18/Ques_174.cpp
@@ -0,0 +1,28 @@
+// Count the Frequency of Each Element in an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 3, 2, 2, 4, 3, 5};
+    int n = 7;
+    bool visited[7] = {false};
+
+    for (int i = 0; i < n; i++) {
+        if (visited[i]) {
+            continue;
+        }
+
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                visited[j] = true;
+            }
+        }
+
+        cout << arr[i] << " occurs " << count << " time(s)" << endl;
+    }
+
+    return 0;
+}
