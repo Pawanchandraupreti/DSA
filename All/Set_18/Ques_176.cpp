53e7dcb059c12fc68bb754e9775016dab742532c
Pawan <upretipawanchandra@gmail.com>
2026-05-06 23:38:43 +0530

    Done

---
+++ All/Set_18/Ques_176.cpp
@@ -0,0 +1,20 @@
+// Find the Largest Element in an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {12, 45, 7, 89, 23};
+    int n = 5;
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest element: " << largest << endl;
+
+    return 0;
+}
