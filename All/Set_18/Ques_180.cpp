65697909fa8ed7fcb2c1244bd04a432fff39c953
Pawan <upretipawanchandra@gmail.com>
2026-05-06 23:38:44 +0530

    Done

---
+++ All/Set_18/Ques_180.cpp
@@ -0,0 +1,37 @@
+// Merge Two Sorted Arrays
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr1[] = {1, 3, 5, 7};
+    int arr2[] = {2, 4, 6, 8};
+    int n1 = 4;
+    int n2 = 4;
+    int merged[8];
+    int i = 0, j = 0, k = 0;
+
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j]) {
+            merged[k++] = arr1[i++];
+        } else {
+            merged[k++] = arr2[j++];
+        }
+    }
+
+    while (i < n1) {
+        merged[k++] = arr1[i++];
+    }
+
+    while (j < n2) {
+        merged[k++] = arr2[j++];
+    }
+
+    cout << "Merged array: ";
+    for (int x = 0; x < k; x++) {
+        cout << merged[x] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
