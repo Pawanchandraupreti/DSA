b0c6ec108b2b670f4895d7c44fc6f7a6322a2e18
Pawan <upretipawanchandra@gmail.com>
2026-05-09 23:16:22 +0530

    Done

---
+++ All/Set_19/Ques_181.cpp
@@ -0,0 +1,25 @@
+// Find the Kth Smallest Element in an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {7, 10, 4, 3, 20, 15};
+    int n = 6;
+    int k = 3;
+    
+    // Simple sorting approach
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    cout << "Kth smallest element (k=" << k << "): " << arr[k - 1] << endl;
+    
+    return 0;
+}
