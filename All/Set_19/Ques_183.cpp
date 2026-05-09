6134de19bc7c2f9702db29c3599b3f96634070af
Pawan <upretipawanchandra@gmail.com>
2026-05-09 23:16:29 +0530

    Done

---
+++ All/Set_19/Ques_183.cpp
@@ -0,0 +1,21 @@
+// Find Missing Number in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 5, 6, 7, 8};
+    int n = 7;
+    int expectedSum = (8 * 9) / 2;  // Sum from 1 to 8
+    int actualSum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        actualSum += arr[i];
+    }
+    
+    int missingNumber = expectedSum - actualSum;
+    
+    cout << "Missing number: " << missingNumber << endl;
+    
+    return 0;
+}
