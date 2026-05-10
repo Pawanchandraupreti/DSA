11240af1b036353b6820174828263e4272f6321d
Pawan <upretipawanchandra@gmail.com>
2026-05-10 23:31:49 +0530

    modified:   All/Set_19/Ques_184.cpp

--- All/Set_19/Ques_184.cpp
+++ All/Set_19/Ques_184.cpp
@@ -6,23 +6,20 @@ using namespace std;
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int n = 5;
-    int k = 2;  // Rotate by 2 positions
+    int k = 2;  
     
-    k = k % n;  // Handle k > n
+    k = k % n;  
     
     int temp[5];
     
-    // Store last k elements
     for (int i = 0; i < k; i++) {
         temp[i] = arr[n - k + i];
     }
     
-    // Shift remaining elements
     for (int i = n - 1; i >= k; i--) {
         arr[i] = arr[i - k];
     }
     
-    // Copy back temp elements
     for (int i = 0; i < k; i++) {
         arr[i] = temp[i];
     }
