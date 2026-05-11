9bfcd1189c1a0a148c779679ea3e3ce62a55845e
Pawan <upretipawanchandra@gmail.com>
2026-05-11 23:28:34 +0530

    Done

---
+++ All/Set_19/Ques_187.cpp
@@ -0,0 +1,30 @@
+// Move All Zeroes to the End
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {0, 1, 0, 3, 12, 0, 5};
+    int n = 7;
+    int index = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[index] = arr[i];
+            index++;
+        }
+    }
+
+    while (index < n) {
+        arr[index] = 0;
+        index++;
+    }
+
+    cout << "Array after moving zeroes: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
