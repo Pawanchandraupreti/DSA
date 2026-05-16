b36f7bbafe430d6683872764ac38266adf09da65
Pawan <upretipawanchandra@gmail.com>
2026-05-16 23:53:55 +0530

    Done

---
+++ All/Set_20/Ques_194.cpp
@@ -0,0 +1,20 @@
+// Find Minimum Element in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {45, 12, 67, 8, 34, 56};
+    int n = 6;
+    int minVal = arr[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < minVal) {
+            minVal = arr[i];
+        }
+    }
+    
+    cout << "Minimum element is: " << minVal << endl;
+    
+    return 0;
+}
