71351f4de74f6efc0dd13be7cd7d127410761f9e
Pawan <upretipawanchandra@gmail.com>
2026-05-16 23:53:44 +0530

    Done

---
+++ All/Set_20/Ques_193.cpp
@@ -0,0 +1,19 @@
+// Find Average of Array Elements
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    
+    double average = (double)sum / n;
+    cout << "Average of array elements: " << average << endl;
+    
+    return 0;
+}
