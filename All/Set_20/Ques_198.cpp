c00323d2a8b7e08b05133e2d6f0db374eab72e81
Pawan <upretipawanchandra@gmail.com>
2026-05-18 23:34:59 +0530

    done Done

---
+++ All/Set_20/Ques_198.cpp
@@ -0,0 +1,18 @@
+// Find Single Number Using XOR
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {4, 1, 2, 1, 2};
+    int n = 5;
+    int uniqueValue = 0;
+
+    for (int i = 0; i < n; i++) {
+        uniqueValue ^= arr[i];
+    }
+
+    cout << "Single number: " << uniqueValue << endl;
+
+    return 0;
+}
