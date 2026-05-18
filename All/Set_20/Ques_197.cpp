eb681b0d7b5dd08c275b5f34e5867afbbe5ff327
Pawan <upretipawanchandra@gmail.com>
2026-05-18 23:32:45 +0530

    Done

---
+++ All/Set_20/Ques_197.cpp
@@ -0,0 +1,26 @@
+// Find Maximum Consecutive Ones
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 1, 0, 1, 1, 1, 0, 1};
+    int n = 8;
+    int currentCount = 0;
+    int maxCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1) {
+            currentCount++;
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+            }
+        } else {
+            currentCount = 0;
+        }
+    }
+
+    cout << "Maximum consecutive ones: " << maxCount << endl;
+
+    return 0;
+}
