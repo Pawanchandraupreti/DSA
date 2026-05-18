93e0b379e59f91fb9abd58e7753f4d1d7d2f7873
Pawan <upretipawanchandra@gmail.com>
2026-05-18 23:32:38 +0530

    Done

---
+++ All/Set_20/Ques_196.cpp
@@ -0,0 +1,29 @@
+// Find Leaders in Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {16, 17, 4, 3, 5, 2};
+    int n = 6;
+    int leaders[6];
+    int count = 0;
+    int maxFromRight = arr[n - 1];
+
+    leaders[count++] = maxFromRight;
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] > maxFromRight) {
+            maxFromRight = arr[i];
+            leaders[count++] = maxFromRight;
+        }
+    }
+
+    cout << "Leaders in array: ";
+    for (int i = count - 1; i >= 0; i--) {
+        cout << leaders[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
