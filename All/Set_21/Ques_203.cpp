5cbe33bd010104fd1529a9980a6b39aa9e6d587e
Pawan <upretipawanchandra@gmail.com>
2026-05-24 23:43:11 +0530

    Done

---
+++ All/Set_21/Ques_203.cpp
@@ -0,0 +1,18 @@
+// Count Inversions (Brute Force)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {2, 4, 1, 3, 5};
+    int n = arr.size();
+    int inv = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) inv++;
+        }
+    }
+    cout << "Number of inversions: " << inv << endl;
+    return 0;
+}
