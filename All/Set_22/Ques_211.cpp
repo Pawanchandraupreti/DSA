36f5aaf605ef1b6ad729131930f121f8eebeff32
Pawan <upretipawanchandra@gmail.com>
2026-05-26 23:54:24 +0530

    Remove generated .exe files (except All/Set_22)

---
+++ All/Set_22/Ques_211.cpp
@@ -0,0 +1,22 @@
+// Palindrome String Check
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(const string &s) {
+    int i = 0, j = s.size() - 1;
+    while (i < j) {
+        if (s[i] != s[j]) return false;
+        i++; j--;
+    }
+    return true;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+    if (isPalindrome(s)) cout << "Palindrome";
+    else cout << "Not Palindrome";
+    return 0;
+}
