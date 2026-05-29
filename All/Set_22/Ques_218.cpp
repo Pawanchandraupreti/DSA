ef1b845b3e67ab380e66a4ae9095d52f42b94d20
Pawan <upretipawanchandra@gmail.com>
2026-05-29 23:34:47 +0530

    Done

---
+++ All/Set_22/Ques_218.cpp
@@ -0,0 +1,16 @@
+// Index of Maximum Element
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n; if(!(cin >> n)) return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    if (n == 0) { cout << -1; return 0; }
+    int idx = 0;
+    for (int i = 1; i < n; ++i) if (a[i] > a[idx]) idx = i;
+    cout << idx;
+    return 0;
+}
