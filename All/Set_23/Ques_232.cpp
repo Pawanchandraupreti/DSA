656a4d25cf8b79363f57eb9b48553688d70c4ecc
Pawan <upretipawanchandra@gmail.com>
2026-06-02 23:45:03 +0530

    Done

---
+++ All/Set_23/Ques_232.cpp
@@ -0,0 +1,35 @@
+// Count occurrences using lower/upper bound
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lowerBound(const vector<int>& a, int x) {
+    int l = 0, r = (int)a.size();
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        if (a[m] < x) l = m + 1;
+        else r = m;
+    }
+    return l;
+}
+
+int upperBound(const vector<int>& a, int x) {
+    int l = 0, r = (int)a.size();
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        if (a[m] <= x) l = m + 1;
+        else r = m;
+    }
+    return l;
+}
+
+int main() {
+    int n; if (!(cin >> n)) return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    int x; cin >> x;
+    int cnt = upperBound(a, x) - lowerBound(a, x);
+    cout << cnt;
+    return 0;
+}
