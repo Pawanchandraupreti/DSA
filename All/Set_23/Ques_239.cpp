970e90c6c2971104cfb047a131511cc596d1b0bb
Pawan <upretipawanchandra@gmail.com>
2026-06-03 23:35:09 +0530

    Done

---
+++ All/Set_23/Ques_239.cpp
@@ -0,0 +1,13 @@
+// Palindrome check for string
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s; if (!(cin >> s)) return 0;
+    string t = s; reverse(t.begin(), t.end());
+    cout << (s == t ? "YES" : "NO");
+    return 0;
+}
