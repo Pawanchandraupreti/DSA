46d9399e2473b1ed57c75b267eec4370c8d6c3db
Pawan <upretipawanchandra@gmail.com>
2026-06-03 23:35:15 +0530

    Done

---
+++ All/Set_23/Ques_240.cpp
@@ -0,0 +1,14 @@
+// Count distinct elements
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    int n; if (!(cin >> n)) return 0;
+    unordered_set<long long> s;
+    for (int i = 0; i < n; ++i) { long long x; cin >> x; s.insert(x); }
+    cout << s.size();
+    return 0;
+}
