d687ea3f1c5e06bb021c69385c53027009bfa257
Pawan <upretipawanchandra@gmail.com>
2026-06-03 23:35:02 +0530

    Done

---
+++ All/Set_23/Ques_238.cpp
@@ -0,0 +1,13 @@
+// Product of array elements
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n; if (!(cin >> n)) return 0;
+    long long prod = 1;
+    for (int i = 0; i < n; ++i) { long long x; cin >> x; prod *= x; }
+    cout << prod;
+    return 0;
+}
