e9dbbae41e051720685cb2f193ba8a8746ad895e
Pawan <upretipawanchandra@gmail.com>
2026-06-05 23:55:42 +0530

    Done

---
+++ All/Set_24/Ques_241.cpp
@@ -0,0 +1,13 @@
+// Sum of digits
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s; if (!(cin >> s)) return 0;
+    int sum = 0;
+    for (char c : s) if (c >= '0' && c <= '9') sum += c - '0';
+    cout << sum;
+    return 0;
+}
