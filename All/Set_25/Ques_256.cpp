fd2190a5305360d87780001854a03c4570a87322
Pawan <upretipawanchandra@gmail.com>
2026-06-10 23:47:12 +0530

    Done Question 256

---
+++ All/Set_25/Ques_256.cpp
@@ -0,0 +1,55 @@
+// Dijkstra shortest path from 1 to n in a weighted graph
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+    vector<vector<pair<int, long long>>> g(n + 1);
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        long long w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
+    }
+
+    const long long INF = (1LL << 62);
+    vector<long long> dist(n + 1, INF);
+    vector<int> parent(n + 1, -1);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+    dist[1] = 0;
+    pq.push({0, 1});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (d != dist[u]) continue;
+        for (auto [v, w] : g[u]) {
+            if (dist[v] > d + w) {
+                dist[v] = d + w;
+                parent[v] = u;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    if (dist[n] == INF) {
+        cout << -1;
+        return 0;
+    }
+
+    cout << dist[n] << '\n';
+    vector<int> path;
+    for (int cur = n; cur != -1; cur = parent[cur]) path.push_back(cur);
+    reverse(path.begin(), path.end());
+    for (size_t i = 0; i < path.size(); ++i) {
+        if (i) cout << ' ';
+        cout << path[i];
+    }
+    return 0;
+}
