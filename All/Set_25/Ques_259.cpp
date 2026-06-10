1e589a122d16eb047ea922a6a4372ebcfa42346e
Pawan <upretipawanchandra@gmail.com>
2026-06-10 23:49:01 +0530

    Done Question 259

---
+++ All/Set_25/Ques_259.cpp
@@ -0,0 +1,29 @@
+// Sliding window maximum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    if (!(cin >> n >> k)) return 0;
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    deque<int> dq;
+    vector<long long> res;
+    for (int i = 0; i < n; ++i) {
+        while (!dq.empty() && dq.front() <= i - k) dq.pop_front();
+        while (!dq.empty() && a[dq.back()] <= a[i]) dq.pop_back();
+        dq.push_back(i);
+        if (i >= k - 1) res.push_back(a[dq.front()]);
+    }
+
+    for (size_t i = 0; i < res.size(); ++i) {
+        if (i) cout << ' ';
+        cout << res[i];
+    }
+    return 0;
+}
