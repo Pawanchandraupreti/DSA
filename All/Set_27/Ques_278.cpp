a29ecdf2c1521623858db92170b592c0ce445ca9
Pawan <upretipawanchandra@gmail.com>
2026-06-16 23:41:37 +0530

    Done Question 278

---
+++ All/Set_27/Ques_278.cpp
@@ -0,0 +1,17 @@
+// Count subsets with sum S (mod 1e9+7)
+
+#include <bits/stdc++.h>
+using namespace std; using ll = long long;
+static const ll MOD = 1000000007LL;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n; int S; if(!(cin>>n>>S)) return 0;
+    vector<int> a(n); for(int i=0;i<n;++i) cin>>a[i];
+    vector<ll> dp(S+1,0); dp[0]=1;
+    for(int x: a){
+        for(int s=S; s>=x; --s) dp[s] = (dp[s] + dp[s-x])%MOD;
+    }
+    cout<<dp[S];
+    return 0;
+}
