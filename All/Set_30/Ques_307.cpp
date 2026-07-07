edf74fe4abdb542912eb66462e9ebb0e1f8c11f6
Pawan <upretipawanchandra@gmail.com>
2026-07-07 23:02:13 +0530

    Done

---
+++ All/Set_30/Ques_307.cpp
@@ -0,0 +1,24 @@
+// Count frequency of each element in an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    unordered_map<long long, int> freq;
+    vector<long long> order;
+
+    for (int i = 0; i < n; ++i) {
+        long long x;
+        cin >> x;
+        if (freq.find(x) == freq.end()) order.push_back(x);
+        ++freq[x];
+    }
+
+    for (long long x : order) {
+        cout << x << ' ' << freq[x] << '\n';
+    }
+    return 0;
+}
