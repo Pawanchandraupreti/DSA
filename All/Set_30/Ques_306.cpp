23482305c13d53823d943d5e545327e9a562a7f8
Pawan <upretipawanchandra@gmail.com>
2026-07-07 23:02:07 +0530

    Done

---
+++ All/Set_30/Ques_306.cpp
@@ -0,0 +1,15 @@
+// Compute GCD and LCM of two numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b;
+    if (!(cin >> a >> b)) return 0;
+
+    long long g = std::gcd(a, b);
+    long long l = (a / g) * b;
+
+    cout << g << '\n' << l;
+    return 0;
+}
