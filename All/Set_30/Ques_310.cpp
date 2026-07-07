079dedc13bec0f75d0bd98fad968bbcbf7ed59e0
Pawan <upretipawanchandra@gmail.com>
2026-07-07 23:02:29 +0530

    Done

---
+++ All/Set_30/Ques_310.cpp
@@ -0,0 +1,32 @@
+// Check if parentheses are balanced
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    if (!(cin >> s)) return 0;
+
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            if (st.empty()) {
+                cout << "No";
+                return 0;
+            }
+            char top = st.top();
+            st.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                cout << "No";
+                return 0;
+            }
+        }
+    }
+
+    cout << (st.empty() ? "Yes" : "No");
+    return 0;
+}
