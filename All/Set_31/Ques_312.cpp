5de4235fe56bd34db03f426ed05c737a332c4120
Pawan <upretipawanchandra@gmail.com>
2026-07-11 23:46:37 +0530

    Done

---
+++ All/Set_31/Ques_312.cpp
@@ -0,0 +1,18 @@
+// Reverse an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    for (int i = n - 1; i >= 0; --i) {
+        if (i < n - 1) cout << ' ';
+        cout << a[i];
+    }
+    return 0;
+}
