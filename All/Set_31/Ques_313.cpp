e28524da52e2c82c69fde8403d1680e72914a8bf
Pawan <upretipawanchandra@gmail.com>
2026-07-11 23:47:53 +0530

    Done

---
+++ All/Set_31/Ques_313.cpp
@@ -0,0 +1,20 @@
+// Find the missing number from 1 to n
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    long long sum = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        long long x;
+        cin >> x;
+        sum += x;
+    }
+
+    long long expected = 1LL * n * (n + 1) / 2;
+    cout << expected - sum;
+    return 0;
+}
