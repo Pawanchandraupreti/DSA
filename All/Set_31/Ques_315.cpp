acef52b47fd862279cef00964b3d22933990e8e8
Pawan <upretipawanchandra@gmail.com>
2026-07-11 23:50:37 +0530

    Done

---
+++ All/Set_31/Ques_315.cpp
@@ -0,0 +1,20 @@
+// Count vowels in a string
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    if (!(cin >> s)) return 0;
+
+    int countVowels = 0;
+    for (char c : s) {
+        char ch = tolower(static_cast<unsigned char>(c));
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            ++countVowels;
+        }
+    }
+
+    cout << countVowels;
+    return 0;
+}
