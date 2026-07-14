5d22d73981f059739e7e7aabe28d3271412e5b6a
Pawan <upretipawanchandra@gmail.com>
2026-07-14 23:16:54 +0530

    Done

---
+++ All/Set_31/Ques_319.cpp
@@ -0,0 +1,25 @@
+// Merge two sorted arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+    vector<long long> a(n), b(m);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int j = 0; j < m; ++j) cin >> b[j];
+    vector<long long> res; res.reserve(n + m);
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (a[i] <= b[j]) res.push_back(a[i++]);
+        else res.push_back(b[j++]);
+    }
+    while (i < n) res.push_back(a[i++]);
+    while (j < m) res.push_back(b[j++]);
+    for (size_t t = 0; t < res.size(); ++t) {
+        if (t) cout << ' ';
+        cout << res[t];
+    }
+    return 0;
+}
