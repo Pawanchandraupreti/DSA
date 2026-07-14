89174e4908f206a513ef8ce4fe54a1cf5a98b984
Pawan <upretipawanchandra@gmail.com>
2026-07-14 23:17:10 +0530

    Done

---
+++ All/Set_31/Ques_320.cpp
@@ -0,0 +1,11 @@
+// Count set bits in a number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unsigned long long x;
+    if (!(cin >> x)) return 0;
+    cout << __builtin_popcountll(x);
+    return 0;
+}
