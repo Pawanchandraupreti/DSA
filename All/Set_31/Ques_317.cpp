62aa2f07980bf41f79c53d5e1921d300dc7e41dc
Pawan <upretipawanchandra@gmail.com>
2026-07-14 23:16:21 +0530

    Done

---
+++ All/Set_31/Ques_317.cpp
@@ -0,0 +1,14 @@
+// Check if a number is a palindrome (integer)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x;
+    if (!(cin >> x)) return 0;
+    if (x < 0) { cout << "No"; return 0; }
+    long long orig = x, rev = 0;
+    while (x) { rev = rev * 10 + (x % 10); x /= 10; }
+    cout << (rev == orig ? "Yes" : "No");
+    return 0;
+}
