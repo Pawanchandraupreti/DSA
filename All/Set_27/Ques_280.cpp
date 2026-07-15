4ac2548d0edce7bd1dc6bbfc694a24c7d35a57c3
Pawan <upretipawanchandra@gmail.com>
2026-07-15 23:24:22 +0530

    Done

--- All/Set_27/Ques_280.cpp
+++ All/Set_27/Ques_280.cpp
@@ -1,15 +1,73 @@
-// Convex hull (Graham scan) output hull points in CCW
+// Eulerian path in directed graph (Hierholzer)
 
 #include <bits/stdc++.h>
-using namespace std; using ll = long long;
-struct P{ ll x,y; };
-ll cross(const P &a,const P &b,const P &c){ return (b.x-a.x)*(c.y-a.y)-(b.y-a.y)*(c.x-a.x); }
+using namespace std;
 
-int main(){ ios::sync_with_stdio(false); cin.tie(nullptr); int n; if(!(cin>>n)) return 0; vector<P> pts(n); for(int i=0;i<n;++i) cin>>pts[i].x>>pts[i].y; sort(pts.begin(), pts.end(), [](const P&a,const P&b){ return a.x<b.x || (a.x==b.x && a.y<b.y); }); if(n<=1){ for(auto &p:pts) cout<<p.x<<" "<<p.y<<"\n"; return 0; }
-    vector<P> lo, hi;
-    for(auto &p: pts){ while(lo.size()>=2 && cross(lo[lo.size()-2], lo.back(), p) <= 0) lo.pop_back(); lo.push_back(p); }
-    for(int i=(int)pts.size()-1;i>=0;--i){ auto &p=pts[i]; while(hi.size()>=2 && cross(hi[hi.size()-2], hi.back(), p) <= 0) hi.pop_back(); hi.push_back(p); }
-    lo.pop_back(); hi.pop_back(); vector<P> hull = lo; hull.insert(hull.end(), hi.begin(), hi.end()); cout<<hull.size()<<"\n"; for(auto &p: hull) cout<<p.x<<" "<<p.y<<"\n"; return 0; }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
 
+    vector<vector<int>> g(n + 1);
+    vector<int> indeg(n + 1, 0), outdeg(n + 1, 0);
+    vector<pair<int, int>> edges(m);
 
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        edges[i] = {u, v};
+        g[u].push_back(v);
+        outdeg[u]++;
+        indeg[v]++;
+    }
+
+    int start = -1;
+    int s1 = 0, s2 = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (outdeg[i] - indeg[i] == 1) {
+            s1++;
+        } else if (indeg[i] - outdeg[i] == 1) {
+            s2++;
+        } else if (indeg[i] != outdeg[i]) {
+            cout << -1;
+            return 0;
+        }
+
+        if (outdeg[i] > 0 && start == -1) start = i;
+    }
+
+    if (!((s1 == 1 && s2 == 1) || (s1 == 0 && s2 == 0))) {
+        cout << -1;
+        return 0;
+    }
+
+    if (start == -1) start = 1;
+
+    vector<int> idx(n + 1, 0), st, path;
+    st.push_back(start);
+
+    while (!st.empty()) {
+        int v = st.back();
+        if (idx[v] < (int)g[v].size()) {
+            st.push_back(g[v][idx[v]++]);
+        } else {
+            path.push_back(v);
+            st.pop_back();
+        }
+    }
+
+    if ((int)path.size() != m + 1) {
+        cout << -1;
+        return 0;
+    }
+
+    reverse(path.begin(), path.end());
+    for (size_t i = 0; i < path.size(); ++i) {
+        if (i) cout << ' ';
+        cout << path[i];
+    }
+
+    return 0;
+}
