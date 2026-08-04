327d80afa381d320e7f7f1d4b0bd9599694c7f80
Pawan <upretipawanchandra@gmail.com>
2026-08-04 23:06:11 +0530

    Ques_322 Done

---
+++ All/Set_32/Ques_322.cpp
@@ -0,0 +1,12 @@
+// Check if a number is even or odd
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x;
+    if (!(cin >> x)) return 0;
+
+    cout << (x % 2 == 0 ? "Even" : "Odd");
+    return 0;
+}
