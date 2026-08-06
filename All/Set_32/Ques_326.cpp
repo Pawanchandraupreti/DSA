f8c1006dc59948be8ccc5118d5692038cc2f2953
Pawan <upretipawanchandra@gmail.com>
2026-08-06 23:51:08 +0530

    Ques_326 Done

---
+++ All/Set_32/Ques_326.cpp
@@ -0,0 +1,21 @@
+// Length of longest increasing subsequence
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<long long> tails;
+    for (int i = 0; i < n; ++i) {
+        long long x;
+        cin >> x;
+        auto it = lower_bound(tails.begin(), tails.end(), x);
+        if (it == tails.end()) tails.push_back(x);
+        else *it = x;
+    }
+
+    cout << tails.size();
+    return 0;
+}
