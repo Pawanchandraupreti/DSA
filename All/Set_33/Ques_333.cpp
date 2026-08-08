2c27f745fc3e6c5c44e9b4dbf93165527c5f0f1f
Pawan <upretipawanchandra@gmail.com>
2026-08-08 23:14:23 +0530

    Ques_333 Done

---
+++ All/Set_33/Ques_333.cpp
@@ -0,0 +1,36 @@
+// Topological sort (print -1 if cycle exists)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+    vector<vector<int>> adj(n+1);
+    vector<int> indeg(n+1, 0);
+    for (int i = 0; i < m; ++i) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        ++indeg[v];
+    }
+
+    queue<int> q;
+    for (int i = 1; i <= n; ++i) if (indeg[i] == 0) q.push(i);
+    vector<int> order;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        order.push_back(u);
+        for (int v : adj[u]) {
+            if (--indeg[v] == 0) q.push(v);
+        }
+    }
+
+    if ((int)order.size() != n) { cout << -1; }
+    else {
+        for (int i = 0; i < n; ++i) {
+            if (i) cout << ' ';
+            cout << order[i];
+        }
+    }
+    return 0;
+}
