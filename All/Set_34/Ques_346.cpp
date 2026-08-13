8254443f92171030e79eff21c1b105471024586e
Pawan <upretipawanchandra@gmail.com>
2026-08-13 23:18:42 +0530

    Ques_346 Done

---
+++ All/Set_34/Ques_346.cpp
@@ -0,0 +1,21 @@
+// Maximum subarray sum using Kadane's algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    long long curr = a[0], best = a[0];
+    for (int i = 1; i < n; ++i) {
+        curr = max(a[i], curr + a[i]);
+        best = max(best, curr);
+    }
+
+    cout << best;
+    return 0;
+}
