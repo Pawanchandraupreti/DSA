7c3888eff5c24106337a4d7e733d2c3093969b45
Pawan <upretipawanchandra@gmail.com>
2026-08-13 23:20:10 +0530

    Ques_349 Done

---
+++ All/Set_34/Ques_349.cpp
@@ -0,0 +1,21 @@
+// Count vowels and consonants in a string
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    if (!getline(cin, s)) return 0;
+
+    int vowels = 0, consonants = 0;
+    for (char c : s) {
+        c = tolower(c);
+        if (c >= 'a' && c <= 'z') {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ++vowels;
+            else ++consonants;
+        }
+    }
+
+    cout << vowels << ' ' << consonants;
+    return 0;
+}
