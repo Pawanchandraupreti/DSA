a6429dc96689a34d0ef9ca2a12544f77f12d07e1
Pawan <upretipawanchandra@gmail.com>
2026-08-18 23:50:52 +0530

    Done

---
+++ All/Set_37/Ques_373.cpp
@@ -0,0 +1,27 @@
+// Find the nth Fibonacci number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long fibonacci(int n) {
+    if (n <= 1) return n;
+
+    long long prev = 0;
+    long long curr = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        long long next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n);
+    return 0;
+}
