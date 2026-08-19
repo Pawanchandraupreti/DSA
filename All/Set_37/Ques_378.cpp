3089362b8190e2c1b3752bd458f6b70eaa692ecc
Pawan <upretipawanchandra@gmail.com>
2026-08-19 23:48:14 +0530

    Ques_378 Done

---
+++ All/Set_37/Ques_378.cpp
@@ -0,0 +1,35 @@
+// Find the maximum number of activities that do not overlap
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maximumActivities(vector<pair<int, int>> activities) {
+    sort(activities.begin(), activities.end(), [](const auto& first, const auto& second) {
+        return first.second < second.second;
+    });
+
+    int count = 0;
+    int lastFinish = INT_MIN;
+
+    for (const auto& activity : activities) {
+        if (activity.first >= lastFinish) {
+            ++count;
+            lastFinish = activity.second;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> activities(n);
+    for (auto& activity : activities) {
+        cin >> activity.first >> activity.second;
+    }
+
+    cout << maximumActivities(activities);
+    return 0;
+}
