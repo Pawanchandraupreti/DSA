d7f86f87a4d1372d0b335e9a6580f36144f15bc4
Pawan <upretipawanchandra@gmail.com>
2026-08-21 23:58:20 +0530

    Done

---
+++ All/Set_38/Ques_384.cpp
@@ -0,0 +1,44 @@
+// Sliding Window Maximum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> maxSlidingWindow(const vector<int>& nums, int k) {
+    deque<int> dq;
+    vector<int> result;
+
+    for (int i = 0; i < (int)nums.size(); ++i) {
+        while (!dq.empty() && dq.front() <= i - k) {
+            dq.pop_front();
+        }
+
+        while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if (i >= k - 1) {
+            result.push_back(nums[dq.front()]);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    vector<int> result = maxSlidingWindow(nums, k);
+    for (int value : result) {
+        cout << value << ' ';
+    }
+
+    return 0;
+}
