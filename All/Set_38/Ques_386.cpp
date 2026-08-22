a3280b70f6b4bc078b6cd6e8a949b7d493b6077b
Pawan <upretipawanchandra@gmail.com>
2026-08-22 22:48:31 +0530

    Done

---
+++ All/Set_38/Ques_386.cpp
@@ -0,0 +1,50 @@
+// N-Queens II
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSafe(const vector<string>& board, int row, int column) {
+    int size = (int)board.size();
+
+    for (int previousRow = 0; previousRow < row; ++previousRow) {
+        for (int previousColumn = 0; previousColumn < size; ++previousColumn) {
+            if (board[previousRow][previousColumn] == 'Q' &&
+                abs(previousRow - row) == abs(previousColumn - column)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void countSolutions(vector<string>& board, int row, int& solutions) {
+    int size = (int)board.size();
+    if (row == size) {
+        ++solutions;
+        return;
+    }
+
+    for (int column = 0; column < size; ++column) {
+        if (isSafe(board, row, column)) {
+            board[row][column] = 'Q';
+            countSolutions(board, row + 1, solutions);
+            board[row][column] = '.';
+        }
+    }
+}
+
+int totalNQueens(int n) {
+    vector<string> board(n, string(n, '.'));
+    int solutions = 0;
+    countSolutions(board, 0, solutions);
+    return solutions;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << totalNQueens(n);
+    return 0;
+}
