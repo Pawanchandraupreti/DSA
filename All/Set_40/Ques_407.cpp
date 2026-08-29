e8079e987e0996c9635e982d2ca625361bba0931
Pawan <upretipawanchandra@gmail.com>
2026-08-29 23:06:20 +0530

    Done

---
+++ All/Set_40/Ques_407.cpp
@@ -0,0 +1,50 @@
+// Binary Tree Closest Value
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    explicit TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+};
+
+int closestValue(TreeNode* root, double target) {
+    int closest = root->val;
+    TreeNode* current = root;
+
+    while (current) {
+        if (abs(current->val - target) < abs(closest - target)) {
+            closest = current->val;
+        }
+
+        if (target < current->val) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    return closest;
+}
+
+TreeNode* buildTree() {
+    int value;
+    cin >> value;
+    if (value == -1) return nullptr;
+
+    TreeNode* node = new TreeNode(value);
+    node->left = buildTree();
+    node->right = buildTree();
+    return node;
+}
+
+int main() {
+    TreeNode* root = buildTree();
+    double target;
+    cin >> target;
+
+    cout << closestValue(root, target);
+    return 0;
+}
