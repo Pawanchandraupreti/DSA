a90c147bd189503c0802938683de4687354e7ad6
Pawan <upretipawanchandra@gmail.com>
2026-08-29 23:06:24 +0530

    Done

---
+++ All/Set_40/Ques_408.cpp
@@ -0,0 +1,35 @@
+// Find K Largest Elements
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findKLargest(vector<int>& numbers, int k) {
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for (int number : numbers) {
+        minHeap.push(number);
+        if ((int)minHeap.size() > k) minHeap.pop();
+    }
+
+    vector<int> result;
+    while (!minHeap.empty()) {
+        result.push_back(minHeap.top());
+        minHeap.pop();
+    }
+
+    sort(result.rbegin(), result.rend());
+    return result;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> numbers(n);
+    for (int& number : numbers) cin >> number;
+
+    for (int element : findKLargest(numbers, k)) {
+        cout << element << ' ';
+    }
+    return 0;
+}
