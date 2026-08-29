2c14dc001f4b266bbf6c1b90350fa0675c39b256
Pawan <upretipawanchandra@gmail.com>
2026-08-29 23:06:02 +0530

    Done

---
+++ All/Set_40/Ques_406.cpp
@@ -0,0 +1,53 @@
+// Median from Data Stream
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MedianFinder {
+private:
+    priority_queue<int> smallerHalf;
+    priority_queue<int, vector<int>, greater<int>> largerHalf;
+
+public:
+    void addNum(int number) {
+        if (smallerHalf.empty() || number <= smallerHalf.top()) {
+            smallerHalf.push(number);
+        } else {
+            largerHalf.push(number);
+        }
+
+        if (smallerHalf.size() > largerHalf.size() + 1) {
+            largerHalf.push(smallerHalf.top());
+            smallerHalf.pop();
+        } else if (largerHalf.size() > smallerHalf.size()) {
+            smallerHalf.push(largerHalf.top());
+            largerHalf.pop();
+        }
+    }
+
+    double findMedian() {
+        if (smallerHalf.size() > largerHalf.size()) {
+            return smallerHalf.top();
+        }
+        return (smallerHalf.top() + largerHalf.top()) / 2.0;
+    }
+};
+
+int main() {
+    int operations;
+    cin >> operations;
+
+    MedianFinder finder;
+    while (operations--) {
+        string command;
+        cin >> command;
+        if (command == "add") {
+            int number;
+            cin >> number;
+            finder.addNum(number);
+        } else {
+            cout << fixed << setprecision(1) << finder.findMedian() << ' ';
+        }
+    }
+    return 0;
+}
