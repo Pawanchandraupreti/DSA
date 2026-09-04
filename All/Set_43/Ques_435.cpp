27e98f323835e4216858766a4fb0d420218969dc
Pawan <upretipawanchandra@gmail.com>
2026-09-04 23:26:55 +0530

    Done

---
+++ All/Set_43/Ques_435.cpp
@@ -0,0 +1,27 @@
+// Count Subarrays with Xor Equal to K
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    
+    unordered_map<int, int> mp;
+    mp[0] = 1;
+    int xor_val = 0, cnt = 0;
+    
+    for (int i = 0; i < n; i++) {
+        xor_val ^= arr[i];
+        if (mp.find(xor_val ^ k) != mp.end()) {
+            cnt += mp[xor_val ^ k];
+        }
+        mp[xor_val]++;
+    }
+    
+    cout << cnt;
+    return 0;
+}
