62507ddc889d396e86b6a12b62336996566f0f7e
Pawan <upretipawanchandra@gmail.com>
2026-09-04 23:27:28 +0530

    Done

---
+++ All/Set_43/Ques_439.cpp
@@ -0,0 +1,34 @@
+// Missing Number Range
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    
+    sort(arr.begin(), arr.end());
+    
+    vector<string> res;
+    
+    if (arr[0] != 0) {
+        if (arr[0] == 1) res.push_back("0");
+        else res.push_back("0->" + to_string(arr[0] - 1));
+    }
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] - arr[i-1] > 1) {
+            if (arr[i-1] + 1 == arr[i] - 1) {
+                res.push_back(to_string(arr[i-1] + 1));
+            } else {
+                res.push_back(to_string(arr[i-1] + 1) + "->" + to_string(arr[i] - 1));
+            }
+        }
+    }
+    
+    for (string s : res) cout << s << " ";
+    return 0;
+}
