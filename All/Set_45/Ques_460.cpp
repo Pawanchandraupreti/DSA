c6dd3f12bc0eda8b13e4cf6f00ee112e148aebcb
Pawan <upretipawanchandra@gmail.com>
2026-09-09 23:40:57 +0530

    Done

---
+++ All/Set_45/Ques_460.cpp
@@ -0,0 +1,23 @@
+// Longest Palindromic Subsequence
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> dp(n, 1);
+
+    for (int i = n - 2; i >= 0; i--) {
+        int prev = 0;
+        for (int j = i + 1; j < n; j++) {
+            int old = dp[j];
+            if (s[i] == s[j]) dp[j] = prev + 2;
+            else dp[j] = max(dp[j], dp[j - 1]);
+            prev = old;
+        }
+    }
+    cout << dp[n - 1];
+    return 0;
+}
