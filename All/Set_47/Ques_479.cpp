5175dd4f721738ef9e0f1f92ed0957a4ae949b78
Pawan <upretipawanchandra@gmail.com>
2026-09-14 23:58:05 +0530

    Done

---
+++ All/Set_47/Ques_479.cpp
@@ -0,0 +1,19 @@
+// Count Complete Tree Nodes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* left;
+    Node* right;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int &x : arr) cin >> x;
+    cout << n << '\n';
+    return 0;
+}
