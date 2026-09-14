d81e3746d03f4a2480c491443a2b57a6615b6b22
Pawan <upretipawanchandra@gmail.com>
2026-09-14 23:58:03 +0530

    Done

---
+++ All/Set_47/Ques_475.cpp
@@ -0,0 +1,23 @@
+// Top K Frequent Elements
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    unordered_map<int, int> freq;
+    for (int x : a) freq[x]++;
+
+    vector<pair<int, int>> v(freq.begin(), freq.end());
+    sort(v.begin(), v.end(), [](auto &p, auto &q) {
+        return p.second > q.second;
+    });
+
+    for (int i = 0; i < min(k, (int)v.size()); i++) cout << v[i].first << ' ';
+    cout << '\n';
+    return 0;
+}
