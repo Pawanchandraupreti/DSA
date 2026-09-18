fedab32c227d10195f3dc6e0144f87e5ec63a863
Pawan <upretipawanchandra@gmail.com>
2026-09-18 23:58:33 +0530

    Done

---
+++ All/Set_48/Ques_490.cpp
@@ -0,0 +1,35 @@
+// Course Schedule
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> graph(n);
+    vector<int> indegree(n);
+    for (int i = 0; i < m; i++) {
+        int course, prerequisite;
+        cin >> course >> prerequisite;
+        graph[prerequisite].push_back(course);
+        indegree[course]++;
+    }
+
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        if (indegree[i] == 0) q.push(i);
+    }
+
+    int done = 0;
+    while (!q.empty()) {
+        int course = q.front();
+        q.pop();
+        done++;
+        for (int next : graph[course]) {
+            if (--indegree[next] == 0) q.push(next);
+        }
+    }
+
+    cout << (done == n ? "true" : "false") << '\n';
+    return 0;
+}
