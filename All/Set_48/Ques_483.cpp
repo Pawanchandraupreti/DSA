02b8e3c3a98f912ed020772e664fea40d5c86e62
Pawan <upretipawanchandra@gmail.com>
2026-09-18 23:58:30 +0530

    Done

---
+++ All/Set_48/Ques_483.cpp
@@ -0,0 +1,31 @@
+// Search in Rotated Sorted Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, target;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+    cin >> target;
+
+    int l = 0, r = n - 1, ans = -1;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (a[mid] == target) {
+            ans = mid;
+            break;
+        }
+        if (a[l] <= a[mid]) {
+            if (a[l] <= target && target < a[mid]) r = mid - 1;
+            else l = mid + 1;
+        } else {
+            if (a[mid] < target && target <= a[r]) l = mid + 1;
+            else r = mid - 1;
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
