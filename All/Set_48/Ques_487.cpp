f317e996c10360a3d81bc446bd144fd051a772a3
Pawan <upretipawanchandra@gmail.com>
2026-09-18 23:58:32 +0530

    Done

---
+++ All/Set_48/Ques_487.cpp
@@ -0,0 +1,20 @@
+// Longest Increasing Subsequence
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), dp;
+    for (int &x : a) cin >> x;
+
+    for (int x : a) {
+        auto it = lower_bound(dp.begin(), dp.end(), x);
+        if (it == dp.end()) dp.push_back(x);
+        else *it = x;
+    }
+
+    cout << dp.size() << '\n';
+    return 0;
+}
