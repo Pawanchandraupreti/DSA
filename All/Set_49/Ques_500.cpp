521feb4b77731b91767b9d8257db05183f4704d1
Pawan <upretipawanchandra@gmail.com>
2026-09-20 23:55:54 +0530

    Done

---
+++ All/Set_49/Ques_500.cpp
@@ -0,0 +1,22 @@
+// Subset Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, target;
+    cin >> n >> target;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    vector<bool> dp(target + 1, false);
+    dp[0] = true;
+    for (int x : a) {
+        for (int s = target; s >= x; s--) {
+            dp[s] = dp[s] || dp[s - x];
+        }
+    }
+
+    cout << (dp[target] ? "true" : "false") << '\n';
+    return 0;
+}
