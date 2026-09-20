40e248acc2cbefb1227b5f19411e5d7b178c07a9
Pawan <upretipawanchandra@gmail.com>
2026-09-20 23:55:53 +0530

    Done

---
+++ All/Set_49/Ques_497.cpp
@@ -0,0 +1,20 @@
+// Best Time to Buy and Sell Stock
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for (int &x : prices) cin >> x;
+
+    int minPrice = INT_MAX, maxProfit = 0;
+    for (int x : prices) {
+        minPrice = min(minPrice, x);
+        maxProfit = max(maxProfit, x - minPrice);
+    }
+
+    cout << maxProfit << '\n';
+    return 0;
+}
