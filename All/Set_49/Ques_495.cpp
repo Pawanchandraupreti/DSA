79d2a3c0288d815cfd4538ea83a496e9a72b22d8
Pawan <upretipawanchandra@gmail.com>
2026-09-20 23:55:52 +0530

    Done

---
+++ All/Set_49/Ques_495.cpp
@@ -0,0 +1,23 @@
+// Jump Game
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    int reach = 0;
+    for (int i = 0; i < n; i++) {
+        if (i > reach) {
+            cout << "false\n";
+            return 0;
+        }
+        reach = max(reach, i + a[i]);
+    }
+
+    cout << "true\n";
+    return 0;
+}
