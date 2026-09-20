467fbf3cc85a05ffacc300edb050657f57ef5705
Pawan <upretipawanchandra@gmail.com>
2026-09-20 23:55:51 +0530

    Done

---
+++ All/Set_49/Ques_494.cpp
@@ -0,0 +1,25 @@
+// Merge Sorted Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int &x : a) cin >> x;
+    for (int &x : b) cin >> x;
+
+    vector<int> merged;
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (a[i] <= b[j]) merged.push_back(a[i++]);
+        else merged.push_back(b[j++]);
+    }
+    while (i < n) merged.push_back(a[i++]);
+    while (j < m) merged.push_back(b[j++]);
+
+    for (int x : merged) cout << x << ' ';
+    cout << '\n';
+    return 0;
+}
