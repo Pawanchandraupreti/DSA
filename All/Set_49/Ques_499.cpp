8fdcf5ff944857821168c5a13feabe279cc7aabb
Pawan <upretipawanchandra@gmail.com>
2026-09-20 23:55:54 +0530

    Done

---
+++ All/Set_49/Ques_499.cpp
@@ -0,0 +1,41 @@
+// MST (Prim's Algorithm)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int, int>>> graph(n);
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+        graph[v].push_back({u, w});
+    }
+
+    vector<int> key(n, INT_MAX), parent(n, -1);
+    vector<bool> inMST(n, false);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    key[0] = 0;
+    pq.push({0, 0});
+
+    while (!pq.empty()) {
+        auto [w, u] = pq.top();
+        pq.pop();
+        if (inMST[u]) continue;
+        inMST[u] = true;
+        for (auto [v, wt] : graph[u]) {
+            if (!inMST[v] && wt < key[v]) {
+                key[v] = wt;
+                parent[v] = u;
+                pq.push({key[v], v});
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int x : key) ans += x;
+    cout << ans << '\n';
+    return 0;
+}
